ae2af047e3fe1ecd00e5d7b6b678502d90e14d72
omrisim210 <omrisim@gmail.com>
2016-10-02 01:38:04 +0300

    Removed chromium stuff

--- .config/chromium/Default/Extensions/pilnojpmdoofaelbinaeodfpjheijkbh/2.2.3_0/js/file_protocol_test.js
+++
@@ -1,6 +0,0 @@
-// Based on http://pastebin.com/3deXunJV because Chromes API dosen't throw errors!
-chrome.extension.onRequest.addListener(function(req, sender, respond) {
-  if (req === 'Ping') {
-    respond('Pong');
-  }
-});
